a337890c3807f90b6f3c3c5a5127ec489beb4d54
taem1n1 <youman7953@naver.com>
2022-06-21 14:19:19 +0900

    initial commit

---
+++ src/main/java/com/java/gof/GofApplication.java
@@ -0,0 +1,13 @@
+package com.java.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/gof/GofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
