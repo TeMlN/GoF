d2c9cf7df7d3dc9f5d3dc6573b2e332ec560625a
taem1n1 <youman7953@naver.com>
2022-06-21 14:37:36 +0900

    제일 쉽게 Singleton pattern을 구현하는 방법

---
+++ src/main/java/com/java/gof/single_ton/Settings.java
@@ -0,0 +1,15 @@
+package com.java.gof.single_ton;
+
+public class Settings {
+
+    private static Settings instance;
+
+    private Settings() {}
+
+    public static Settings getInstance() {
+        if (instance == null) {
+            instance = new Settings();
+        }
+        return instance;
+    }
+}
