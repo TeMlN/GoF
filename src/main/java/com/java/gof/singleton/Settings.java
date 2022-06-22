f04e9421bffe8eb183e36bffda10590902c07bfb
taem1n1 <youman7953@naver.com>
2022-06-22 17:48:46 +0900

    직렬화 역직렬화를 이용한 싱글턴 패턴 부수기를 막는 방법

--- src/main/java/com/java/gof/singleton/Settings.java
+++ src/main/java/com/java/gof/singleton/Settings.java
@@ -22,4 +22,14 @@ public class Settings implements Serializable {
     public static Settings getInstance() {
         return SettingsHolder.INSTANCE;
     }
+
+    /**
+     * Serializable과 Deserializable를 이용한 singleton pattern을 깨뜨리는 방법을 무효화 시키는 방법
+     * Deserializable 할때 readResolve라는 signature를 사용하기 때문에 기존의 readResolve는 new 를 통해 인스턴스를 생성하지만
+     * 그걸 조작함으로써 readResolve를 호출해도 getInstance 메소드를 호출하게 변경하면 무효화 시킬 수 있다.
+     * @return
+     */
+    protected Object readResolve() {
+        return getInstance();
+    }
 }
