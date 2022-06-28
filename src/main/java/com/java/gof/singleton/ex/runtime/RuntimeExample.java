51748ea83dee36e2cddc4c07a60312fc970306a0
taem1n1 <youman7953@naver.com>
2022-06-28 22:55:13 +0900

    Singleton pattern이 사용되는 예제

---
+++ src/main/java/com/java/gof/singleton/ex/runtime/RuntimeExample.java
@@ -0,0 +1,13 @@
+package com.java.gof.singleton.ex.runtime;
+
+public class RuntimeExample {
+    public static void main(String[] args) {
+
+        // Runtime 클래스에서 static eager initialization 으로 Runtime 인스턴스를 생성함
+        // new 생성자() 사용불가능
+        Runtime runtime = Runtime.getRuntime();
+
+        System.out.println(runtime.maxMemory());
+        System.out.println(runtime.freeMemory());
+    }
+}
---
+++ src/main/java/com/java/gof/singleton/ex/spring/SpringExample.java
@@ -0,0 +1,6 @@
+package com.java.gof.singleton.ex.spring;
+
+public class SpringExample {
+    public static void main(String[] args) {
+    }
+}
