b95a6187429331cc23d3421551418a1a964b261b
taem1n1 <youman7953@naver.com>
2022-06-30 16:40:22 +0900

    Prototype pattern 구현

---
+++ src/main/java/com/java/gof/creational_patterns/prototype/GithubIssue.java
@@ -0,0 +1,72 @@
+package com.java.gof.creational_patterns.prototype;
+
+import java.util.Objects;
+
+public class GithubIssue implements Cloneable {
+
+    private int id;
+
+    private String title;
+
+    private GithubRepository repository;
+
+    public GithubIssue(GithubRepository repository) {
+        this.repository = repository;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public GithubRepository getRepository() {
+        return repository;
+    }
+
+    public String getUrl() {
+        return String.format("https://github.com/%s/%s/issues/%d",
+                repository.getUser(),
+                repository.getName(),
+                this.getId());
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        GithubRepository repository = new GithubRepository();
+        repository.setUser("whiteship");
+        repository.setName("live-study");
+
+        GithubIssue githubIssue = new GithubIssue(repository);
+        githubIssue.setId(1);
+        githubIssue.setTitle("1주차 과제: JVM은 무엇이며 자바 코드는 어떻게 실행하는 것인가.");
+        return githubIssue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GithubIssue that = (GithubIssue) o;
+        return id == that.id && Objects.equals(title, that.title) && Objects.equals(repository, that.repository);
+    }
+
+    @Override
+    public String toString() {
+        return "GithubIssue{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", repository=" + repository +
+                '}';
+    }
+}
